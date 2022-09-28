f6bc1093d847329874d352e4a3f84813ac9a92f5
Matues <mateus.q.d@gmail.com>
2022-09-28 12:54:36 -0300

    React Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react"; // enportar a biblioteca do react
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <p>Teste2</p>
+            <img src="https://static1.patasdacasa.com.br/articles/6/25/6/@/23466-o-border-collie-e-conhecido-por-ser-a-ra-opengraph_1200-2.jpg" 
+            alt="imagem tela inicial" className="img" />
+        </>
+    )
+
+}
+export default Home;
