abf389a2b0b77e9ecea4e924b59ec3abb850e439
Matues <mateus.q.d@gmail.com>
2022-09-29 15:28:44 -0300

    Implementação Navbar/Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Home from './paginas/home/Home';
+import Navbar from './Components/Estaticos/navbar/Navbar';
+import Footer from './Components/Estaticos/footer/Footer';
 import './App.css';
+import { Grid } from '@material-ui/core'
 
 function App() {
   return (
     <>
-    <Home/>
-    </>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+      
+      </>
   );
 }
 
